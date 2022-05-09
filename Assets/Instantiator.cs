dfc4b9ee52bf80dea7cee2511bd72e469daf9ef5
mateopuli <101335430+mateopuli@users.noreply.github.com>
2022-05-09 11:45:19 -0300

    cube cloned

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public void CloneObject()
+    {
+        Instantiate(objectToClone);
+    }
+}
